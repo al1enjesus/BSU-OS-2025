11f9e4ea6fd88a089305bf00ed32f8960c235c30
somerandomprog <srp981680@gmail.com>
2025-09-14 19:50:56 +0300

    lab1: Будник Михаил - add run script

---
+++ lab1/gr9sub2/БУДНИК_МИХАИЛ/src/run.sh
@@ -0,0 +1,9 @@
+echo "задача А..."
+sh scripts/a.sh
+echo "-----------------"
+echo "задача Б..."
+sh scripts/b.sh
+echo "-----------------"
+echo "задача В..."
+sh scripts/c.sh
+echo "-----------------"
