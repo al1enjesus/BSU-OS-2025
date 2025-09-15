814c22f26882021972c47df541c1553d1b66b76a
Kirill Kuharev <vectordifenstyeeder@gmail.com>
2025-09-15 22:08:11 +0300

    Lab1v2

--- lab1/gr9sub2/Кухарев_Кирилл/script3.sh
+++ lab1/gr9sub2/Кухарев_Кирилл/script3.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Скрипт для вывода топ-5 самых часто устанавливаемых пакетов в Arch Linux
 
 LOG_FILE="/var/log/pacman.log"
 OUTPUT="top5_packages.txt"
@@ -7,8 +6,6 @@ grep "installed" "$LOG_FILE" > installs.txt
 
 awk '{print $4}' installs.txt | sort | uniq -c | sort -nr | head -n 5 > "$OUTPUT"
 
-# Вывод результата
 cat "$OUTPUT"
 
-# Очистка
 rm installs.txt
