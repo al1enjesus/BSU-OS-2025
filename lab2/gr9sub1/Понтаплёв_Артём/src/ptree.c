eba3d665f907dfd510385cd59be3ba18bb063f8f
temka-a <artempontaplev2@gmail.com>
2025-09-21 15:55:13 +0000

    lab2: второе решение

--- lab2/gr9sub1/Понтаплёв_Артём/src/ptree.c
+++ lab2/gr9sub1/Понтаплёв_Артём/src/ptree.c
@@ -16,48 +16,56 @@ char name[50] = "unknown";
 int ppid = -1;
 sprintf(path, "/proc/%d/comm", pid);
 f = fopen(path, "r");
-if (f)
+if (f == NULL)
 {
-if (fgets(name, sizeof(name), f))
+strcpy(name, "unknown");
+}
+else
+{
+if (fgets(name, sizeof(name), f) == NULL)
+{
+strcpy(name, "unknown");
+}
+else
 {
 name[strcspn(name, "\n")] = '\0';
 }
 fclose(f);
 }
-  if (first)
-  {
-  printf("%s(%d)", name, pid);
-  first = 0;
-  }
-  else
-  {
-  printf(" <-%s(%d)", name, pid);
-  }
-  if (pid == 1)
-  {
-  break;
-  }
+if (first)
+{
+printf("%s(%d)", name, pid);
+first = 0;
+}
+else
+{
+printf(" <-%s(%d)", name, pid);
+}
+if (pid == 1)
+{
+break;
+}
 sprintf(path, "/proc/%d/status", pid);
 f = fopen(path, "r");
 if (!f)
 {
 break;
 }
- while (fgets(line, sizeof(line), f))
- {
- if (strncmp(line, "PPid:", 5) == 0)
- {
- ppid = atoi(line + 5);
- break;
- }
- }
- fclose(f);
+while (fgets(line, sizeof(line), f))
+{
+if (strncmp(line, "PPid:", 5) == 0)
+{
+ppid = atoi(line + 5);
+break;
+}
+}
+fclose(f);
 if (ppid <= 0)
 {
 break;
 }
 pid = ppid;
 }
- printf("\n");
- return 0;
- }
+printf("\n");
+return 0;
+}
