855b3f4df95a5d5906806ef2e5b3152934980710
Darkinfect <vectordifenstyeeder@gmail.com>
2025-09-21 19:15:07 +0300

    gr9-sub2-lab2-kuharevKS-v2

--- lab2/gr9sub2/Кухарев_Кирилл/scripts/s.sh
+++ lab2/gr9sub2/Кухарев_Кирилл/scripts/s.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-read -p "Введите PID процесса " name
-echo "Статус процесса"
-cat /proc/$name/cmdline | tr '\0' ' '; echo
-head -n 20 /proc/$name/status
-ls -l /proc/$name/fd
+read -p "Введите PID процесса: " pid
+if [ ! -d "/proc/$pid" ]; then
+    echo "Ошибка: Процесс с PID $pid не существует."
+    exit 1
+fi
+echo "Статус процесса:"
+cat /proc/$pid/cmdline | tr '\0' ' '; echo
+head -n 20 /proc/$pid/status
+ls -l /proc/$pid/fd
+
