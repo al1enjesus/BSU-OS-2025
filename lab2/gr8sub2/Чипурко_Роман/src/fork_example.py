32ee980d21215a84c8053b60ed671c661dc7c9a4
Paprika991 <romachipurko@gmail.com>
2025-09-22 21:24:54 +0300

    Solution

---
+++ lab2/gr8sub2/Чипурко_Роман/src/fork_example.py
@@ -0,0 +1,23 @@
+import multiprocessing
+import os
+import time
+
+def child_proccessing(index):
+    pid = os.getpid()
+    ppid = os.getppid()
+    print(f"Child_{index}: PID = {pid}, PPID = {ppid}", flush = True)
+    time.sleep(10)
+
+if __name__ == "__main__":
+    print("Родитель начинает работать. PID родителя =", os.getpid(), flush = True)
+    processes = []
+    for i in range(1, 3):
+        p = multiprocessing.Process(target=child_proccessing, args=(i,))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join() # = wait()
+        print(f"Дочерний процесс {p.pid} завершился с кодом: {p.exitcode}", flush = True)
+
+print(f"Ожидание родителя своих дочерних процессов завершилось.")
