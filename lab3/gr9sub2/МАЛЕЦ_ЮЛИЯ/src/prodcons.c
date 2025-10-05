308ffa03b2ad59f2b6fd3b821f686d159d0b3284
JustLiat <jullia.minsk@gmail.com>
2025-10-05 20:04:54 +0300

    Update prodcons.c

--- lab3/gr9sub2/МАЛЕЦ_ЮЛИЯ/src/prodcons.c
+++ lab3/gr9sub2/МАЛЕЦ_ЮЛИЯ/src/prodcons.c
@@ -1,263 +1,198 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
+#include <getopt.h>
 
 typedef struct {
-    int *buffer;
+    int* data;
     int capacity;
-    int count;
     int head;
     int tail;
-    int done_producing;
+    int count;
     pthread_mutex_t mutex;
     pthread_cond_t not_full;
     pthread_cond_t not_empty;
+    int producers_active;
 } ring_buffer_t;
 
 typedef struct {
-    ring_buffer_t *rb;
+    ring_buffer_t* rb;
     int items_to_produce;
-    int id;
-} producer_arg_t;
+    int producer_index;
+} producer_args_t;
 
 typedef struct {
-    ring_buffer_t *rb;
-    int id;
-} consumer_arg_t;
+    ring_buffer_t* rb;
+    long long consumed_sum;
+    long long consumed_count;
+    int consumer_index;
+} consumer_args_t;
 
-int rb_init(ring_buffer_t *rb, int capacity) {
-    rb->buffer = malloc(sizeof(int) * capacity);
-    if (!rb->buffer) {
-        return -1;
-    }
+static void rb_init(ring_buffer_t* rb, int capacity, int producers_total) {
+    rb->data = (int*)malloc(sizeof(int) * capacity);
     rb->capacity = capacity;
-    rb->count = 0;
     rb->head = 0;
     rb->tail = 0;
-    rb->done_producing = 0;
-    
-    if (pthread_mutex_init(&rb->mutex, NULL) != 0) {
-        free(rb->buffer);
-        return -1;
-    }
-    
-    if (pthread_cond_init(&rb->not_full, NULL) != 0) {
-        pthread_mutex_destroy(&rb->mutex);
-        free(rb->buffer);
-        return -1;
-    }
-    
-    if (pthread_cond_init(&rb->not_empty, NULL) != 0) {
-        pthread_mutex_destroy(&rb->mutex);
-        pthread_cond_destroy(&rb->not_full);
-        free(rb->buffer);
-        return -1;
-    }
-    
-    return 0;
+    rb->count = 0;
+    rb->producers_active = producers_total;
+    pthread_mutex_init(&rb->mutex, NULL);
+    pthread_cond_init(&rb->not_full, NULL);
+    pthread_cond_init(&rb->not_empty, NULL);
 }
 
-void rb_destroy(ring_buffer_t *rb) {
-    free(rb->buffer);
+static void rb_destroy(ring_buffer_t* rb) {
+    free(rb->data);
     pthread_mutex_destroy(&rb->mutex);
     pthread_cond_destroy(&rb->not_full);
     pthread_cond_destroy(&rb->not_empty);
 }
 
-void *producer(void *arg) {
-    producer_arg_t *parg = (producer_arg_t *)arg;
-    for (int i = 0; i < parg->items_to_produce; ++i) {
-        if (pthread_mutex_lock(&parg->rb->mutex) != 0) {
-            break;
-        }
-        
-        while (parg->rb->count == parg->rb->capacity) {
-            if (pthread_cond_wait(&parg->rb->not_full, &parg->rb->mutex) != 0) {
-                pthread_mutex_unlock(&parg->rb->mutex);
-                break;
-            }
-        }
-
-        int item = i + parg->id * 1000;
-        parg->rb->buffer[parg->rb->tail] = item;
-        parg->rb->tail = (parg->rb->tail + 1) % parg->rb->capacity;
-        parg->rb->count++;
-
-        printf("Producer %d produced %d\n", parg->id, item);
-
-        pthread_cond_signal(&parg->rb->not_empty);
-        pthread_mutex_unlock(&parg->rb->mutex);
+static void rb_push(ring_buffer_t* rb, int value) {
+    pthread_mutex_lock(&rb->mutex);
+    while (rb->count == rb->capacity) {
+        pthread_cond_wait(&rb->not_full, &rb->mutex);
     }
+    rb->data[rb->tail] = value;
+    rb->tail = (rb->tail + 1) % rb->capacity;
+    rb->count++;
+    pthread_cond_signal(&rb->not_empty);
+    pthread_mutex_unlock(&rb->mutex);
+}
 
-    if (pthread_mutex_lock(&parg->rb->mutex) == 0) {
-        parg->rb->done_producing++;
-        pthread_cond_broadcast(&parg->rb->not_empty);
-        pthread_mutex_unlock(&parg->rb->mutex);
+static int rb_pop(ring_buffer_t* rb, int* value) {
+    pthread_mutex_lock(&rb->mutex);
+    while (rb->count == 0 && rb->producers_active > 0) {
+        pthread_cond_wait(&rb->not_empty, &rb->mutex);
     }
+    if (rb->count == 0 && rb->producers_active == 0) {
+        pthread_mutex_unlock(&rb->mutex);
+        return 0;
+    }
+    *value = rb->data[rb->head];
+    rb->head = (rb->head + 1) % rb->capacity;
+    rb->count--;
+    pthread_cond_signal(&rb->not_full);
+    pthread_mutex_unlock(&rb->mutex);
+    return 1;
+}
 
-    printf("Producer %d finished\n", parg->id);
+static void rb_producer_done(ring_buffer_t* rb) {
+    pthread_mutex_lock(&rb->mutex);
+    rb->producers_active--;
+    if (rb->producers_active == 0) {
+        pthread_cond_broadcast(&rb->not_empty);
+    }
+    pthread_mutex_unlock(&rb->mutex);
+}
+
+static void* producer_thread(void* arg) {
+    producer_args_t* a = (producer_args_t*)arg;
+    for (int i = 0; i < a->items_to_produce; i++) {
+        int value = (a->producer_index + 1) * 1000000 + i;
+        rb_push(a->rb, value);
+        printf("Producer %d produced %d\n", a->producer_index, value);
+    }
+    rb_producer_done(a->rb);
+    printf("Producer %d finished\n", a->producer_index);
     return NULL;
 }
 
-void *consumer(void *arg) {
-    consumer_arg_t *carg = (consumer_arg_t *)arg;
-    while (1) {
-        if (pthread_mutex_lock(&carg->rb->mutex) != 0) {
-            break;
-        }
-        
-        while (carg->rb->count == 0) {
-            if (carg->rb->done_producing > 0) {
-                pthread_mutex_unlock(&carg->rb->mutex);
-                printf("Consumer %d finished\n", carg->id);
-                return NULL;
-            }
-            if (pthread_cond_wait(&carg->rb->not_empty, &carg->rb->mutex) != 0) {
-                pthread_mutex_unlock(&carg->rb->mutex);
-                return NULL;
-            }
-        }
-
-        int item = carg->rb->buffer[carg->rb->head];
-        carg->rb->head = (carg->rb->head + 1) % carg->rb->capacity;
-        carg->rb->count--;
-
-        pthread_cond_signal(&carg->rb->not_full);
-        pthread_mutex_unlock(&carg->rb->mutex);
-
-        printf("Consumer %d consumed %d\n", carg->id, item);
+static void* consumer_thread(void* arg) {
+    consumer_args_t* a = (consumer_args_t*)arg;
+    int v;
+    while (rb_pop(a->rb, &v)) {
+        a->consumed_sum += v;
+        a->consumed_count += 1;
+        printf("Consumer %d consumed %d\n", a->consumer_index, v);
     }
+    printf("Consumer %d finished\n", a->consumer_index);
     return NULL;
 }
 
-int main(int argc, char *argv[]) {
-    int P = 0, C = 0, N = 0, B = 0;
-    
-    for (int i = 1; i < argc; i++) {
-        if (argv[i][0] == '-') {
-            switch (argv[i][1]) {
-                case 'P':
-                    if (i + 1 < argc) {
-                        P = atoi(argv[++i]);
-                    } else {
-                        fprintf(stderr, "Option -P requires an argument\n");
-                        return 1;
-                    }
-                    break;
-                case 'C':
-                    if (i + 1 < argc) {
-                        C = atoi(argv[++i]);
-                    } else {
-                        fprintf(stderr, "Option -C requires an argument\n");
-                        return 1;
-                    }
-                    break;
-                case 'N':
-                    if (i + 1 < argc) {
-                        N = atoi(argv[++i]);
-                    } else {
-                        fprintf(stderr, "Option -N requires an argument\n");
-                        return 1;
-                    }
-                    break;
-                case 'B':
-                    if (i + 1 < argc) {
-                        B = atoi(argv[++i]);
-                    } else {
-                        fprintf(stderr, "Option -B requires an argument\n");
-                        return 1;
-                    }
-                    break;
-                default:
-                    fprintf(stderr, "Unknown option: %s\n", argv[i]);
-                    fprintf(stderr, "Usage: %s -P <producers> -C <consumers> -N <items> -B <buffer_size>\n", argv[0]);
-                    return 1;
-            }
-        } else {
-            fprintf(stderr, "Invalid argument: %s\n", argv[i]);
-            fprintf(stderr, "Usage: %s -P <producers> -C <consumers> -N <items> -B <buffer_size>\n", argv[0]);
-            return 1;
+static void usage(const char* prog) {
+    fprintf(stderr, "Usage: %s -P <producers> -C <consumers> -N <items_total> -B <buffer_size>\n", prog);
+}
+
+int main(int argc, char** argv) {
+    int P = 2, C = 2, B = 64;
+    long long N = 100000;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "P:C:N:B:")) != -1) {
+        switch (opt) {
+            case 'P': P = atoi(optarg); break;
+            case 'C': C = atoi(optarg); break;
+            case 'N': N = atoll(optarg); break;
+            case 'B': B = atoi(optarg); break;
+            default: usage(argv[0]); return 1;
         }
     }
-    
-    if (P <= 0 || C <= 0 || N <= 0 || B <= 0) {
-        fprintf(stderr, "Usage: %s -P <producers> -C <consumers> -N <items> -B <buffer_size>\n", argv[0]);
-        fprintf(stderr, "All values must be positive integers\n");
+    if (P <= 0 || C <= 0 || B <= 0 || N < 0) {
+        usage(argv[0]);
         return 1;
     }
-
-    printf("Starting producer-consumer simulation:\n");
-    printf("  Producers: %d\n", P);
-    printf("  Consumers: %d\n", C);
-    printf("  Items per producer: %d\n", N / P);
-    printf("  Total items: %d\n", N);
-    printf("  Buffer size: %d\n", B);
-    printf("\n");
 
     ring_buffer_t rb;
-    if (rb_init(&rb, B) != 0) {
-        fprintf(stderr, "Failed to initialize ring buffer\n");
+    rb_init(&rb, B, P);
+
+    pthread_t* pt = (pthread_t*)calloc((size_t)P, sizeof(pthread_t));
+    pthread_t* ct = (pthread_t*)calloc((size_t)C, sizeof(pthread_t));
+    producer_args_t* pargs = (producer_args_t*)calloc((size_t)P, sizeof(producer_args_t));
+    consumer_args_t* cargs = (consumer_args_t*)calloc((size_t)C, sizeof(consumer_args_t));
+    if (!pt || !ct || !pargs || !cargs) {
+        fprintf(stderr, "Allocation failed\n");
         return 1;
     }
 
-    pthread_t producers[P], consumers[C];
-    producer_arg_t pargs[P];
-    consumer_arg_t cargs[C];
+    int per_producer = (int)(N / P);
+    int remainder = (int)(N % P);
 
-    for (int i = 0; i < P; ++i) {
+    for (int i = 0; i < P; i++) {
         pargs[i].rb = &rb;
-        pargs[i].items_to_produce = N / P;
-        pargs[i].id = i;
-        if (pthread_create(&producers[i], NULL, producer, &pargs[i]) != 0) {
-            fprintf(stderr, "Failed to create producer thread %d\n", i);
-            rb.done_producing = P;
-            pthread_cond_broadcast(&rb.not_empty);
-            
-            for (int j = 0; j < i; ++j) {
-                pthread_join(producers[j], NULL);
-            }
-            for (int j = 0; j < C; ++j) {
-                pthread_join(consumers[j], NULL);
-            }
-            rb_destroy(&rb);
+        pargs[i].producer_index = i;
+        pargs[i].items_to_produce = per_producer + (i < remainder ? 1 : 0);
+        if (pthread_create(&pt[i], NULL, producer_thread, &pargs[i]) != 0) {
+            fprintf(stderr, "pthread_create producer failed\n");
             return 1;
         }
     }
 
-    for (int i = 0; i < C; ++i) {
+    for (int i = 0; i < C; i++) {
         cargs[i].rb = &rb;
-        cargs[i].id = i;
-        if (pthread_create(&consumers[i], NULL, consumer, &cargs[i]) != 0) {
-            fprintf(stderr, "Failed to create consumer thread %d\n", i);
-            rb.done_producing = P;
-            pthread_cond_broadcast(&rb.not_empty);
-            
-            for (int j = 0; j < P; ++j) {
-                pthread_join(producers[j], NULL);
-            }
-            for (int j = 0; j < i; ++j) {
-                pthread_join(consumers[j], NULL);
-            }
-            for (int j = i; j < C; ++j) {
-                pthread_cancel(consumers[j]);
-            }
-            rb_destroy(&rb);
+        cargs[i].consumed_sum = 0;
+        cargs[i].consumed_count = 0;
+        cargs[i].consumer_index = i;
+        if (pthread_create(&ct[i], NULL, consumer_thread, &cargs[i]) != 0) {
+            fprintf(stderr, "pthread_create consumer failed\n");
             return 1;
         }
     }
 
-    for (int i = 0; i < P; ++i) {
-        pthread_join(producers[i], NULL);
+    long long produced_total = 0;
+    for (int i = 0; i < P; i++) {
+        pthread_join(pt[i], NULL);
+        produced_total += pargs[i].items_to_produce;
+    }
+    for (int i = 0; i < C; i++) {
+        pthread_join(ct[i], NULL);
     }
 
-    for (int i = 0; i < C; ++i) {
-        pthread_join(consumers[i], NULL);
+    long long consumed_total = 0;
+    long long consumed_sum = 0;
+    for (int i = 0; i < C; i++) {
+        consumed_total += cargs[i].consumed_count;
+        consumed_sum += cargs[i].consumed_sum;
     }
 
+    printf("[prodcons] (samples skeleton) P=%d C=%d N=%lld B=%d produced=%lld consumed=%lld sum=%lld\n",
+           P, C, N, B, produced_total, consumed_total, consumed_sum);
+
+    free(pt);
+    free(ct);
+    free(pargs);
+    free(cargs);
     rb_destroy(&rb);
-    
-    printf("\nAll producers and consumers finished successfully.\n");
     return 0;
 }
