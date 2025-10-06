2cf58aafff7c49a3be1443e5ee68177b793f06d5
builtInGod <donpaliy9@gmail.com>
2025-10-06 05:33:03 +0300

    Update scriptC.sh

--- lab1/gr9sub1/Rozhdaev_Egor/scriptC.sh
+++ lab1/gr9sub1/Rozhdaev_Egor/scriptC.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+if [ ! -f /var/log/syslog ]; then
+  echo "Ошибка: файл /var/log/syslog не найден"
+  exit 1
+fi
 grep 'install ' /var/log/dpkg.log | \
 awk '{print $4}' | \
 sort | uniq -c | sort -nr | head -n 10
