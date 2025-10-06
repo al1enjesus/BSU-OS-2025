aa69ba5164bb35d7fea8c9d7170b1315ec52de86
builtInGod <donpaliy9@gmail.com>
2025-10-06 05:28:24 +0300

    Update scriptB.sh

--- lab1/gr9sub1/Rozhdaev_Egor/scriptB.sh
+++ lab1/gr9sub1/Rozhdaev_Egor/scriptB.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+if [ ! -f /var/log/syslog ]; then
+  echo "Ошибка: файл /var/log/syslog не найден"
+  exit 1
+fi
 grep -E '(Failed|Invalid)' /var/log/auth.log | \
 grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' | \
 sed -E 's/([0-9]+\.[0-9]+\.[0-9]+\.)[0-9]+/\1x/g' | \
