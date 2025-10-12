4a1fcf3188ca0ff27ade4721c27f227b10c371e6
Anya6699 <anya.daskevich.06@gmail.com>
2025-10-12 22:30:17 +0300

    practice-virus-watchdog: Дашкевич Анна 1.0

---
+++ practice-virus-watchdog/watchdog.py
@@ -0,0 +1,9 @@
+import subprocess
+
+while True:
+    try:
+        pid = subprocess.check_output(['pgrep', 'firefox']).decode('utf-8').replace('\n', '')
+        subprocess.run(['kill', '-KILL', pid])
+        subprocess.run(['zenity', '--error', '--text', 'net net!!'])
+    except:
+        pass
