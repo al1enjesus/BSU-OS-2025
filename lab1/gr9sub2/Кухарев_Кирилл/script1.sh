69173258bd26f5314872efe9280a65bc1bbc68a8
Sancher <mihroniksancher@mail.ru>
2025-11-22 18:32:48 +0000

    lab5: Михальчук Александр

--- lab1/gr9sub2/Кухарев_Кирилл/script1.sh
+++
@@ -1,7 +0,0 @@
-journalctl -p err | 
-     tr -cs '[:alnum:]' '\n' |
-     tr '[:upper:]' '[:lower:]' |
-     sort |
-     uniq -c |
-     sort -nr | 
-     head -n 5
--- practice-virus-watchdog/watchdog.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while true; do
-    pid=$(pgrep firefox)
-    [ -n "$pid" ] && kill $pid &&  echo "hohoho"
-    sleep 2
-done
